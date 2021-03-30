ca98a969dda8487a58b7ca103c6fd6404c9effcf
mahitha3009 <mahibalumuri@gmail.com>
2021-03-30 17:16:00 +0530

    Importing excel and csv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import * as XLSX from 'xlsx';
+const { read, write, utils } = XLSX;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'importExcelCsv';
+  name= 'Angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SheetComponent } from './sheet/sheet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SheetComponent
   ],
   imports: [
     BrowserModule,
